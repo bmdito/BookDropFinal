f1c9462447cc76dd576e2d1d952ea42fc22677e3
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-08-07 18:31:20 -0700

    get all profiles and profile by user id added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -106,4 +106,49 @@ router.post('/', [auth, [
 
 );
 
+//@route    GET api/profile
+//@desc     Get all profiles
+//@access   Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find({}).populate('user', ['name', 'avatar']);
+
+        res.json(profiles);
+
+    } catch (err) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+//@route    GET api/profile/user/:user_id
+//@desc     Get all profiles
+//@access   Public
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({
+            msg: 'There is no profile for this user'
+        });
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+
+        // if equal to Object id (type of error)
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({
+                msg: 'Profile not found'
+            });
+        }
+        res.status(500).send('Server Error')
+    }
+});
+
 module.exports = router;
